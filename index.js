fe66b7a76e768bfe1cdd6d334eb7cd5ba58fb4d7
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-08-23 12:51:45 +0200

    Fixed the json file
    Fixed so that the title and its content shows together in the json file

--- index.js
+++ index.js
@@ -4,39 +4,36 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 request('https://en.wikipedia.org/wiki/Big_cat', (error, response,
- html) => {
-if( !error && response.statusCode == 200){
-     const $ = cheerio.load(html);
+  html) => {
+  if (!error && response.statusCode == 200) {
+    const $ = cheerio.load(html);
 
- var title, content;
- var json = { title : "", content : ""};
-    
-  $('.firstHeading').filter(function(){
-  const siteHeading = $('.firstHeading');
-  title = siteHeading.find('h1').text();
-  content = siteHeading.children('h1').next().text();
-    
-  json.title = title;
-  json.content = content;
-  })
-    
+    const body = $('.mw-parser-output').children();
 
-$('#mw-content-text').each((i, el) => {
-    const title = $(el).find('.mw-headline').text();
-    const content = $(el).find('p').text();
-    
-    json.title = title;
-    json.content = content;
-          
+    const data = [];
+
+    body.each((i, el) => {
+      const siteHeading = $('.firstHeading').text();
+      const title = $(el).filter('h2');
+      const content = $(title).nextUntil('h2', 'p').text();
+
+      data[i] = {
+        heading: siteHeading,
+        title: title.text().trim(),
+        content: content
+      }
+
+      // console.log(data);
+
+    })
+    const jsonFile = JSON.stringify(data);
+
+    fs.writeFile('wiki.json', jsonFile, function (err) {
+      console.log('The data has been saved in a file! Look for the wiki.json file');
+    })
+  } else {
+    console.log('Something is wrong !');
+  }
 
 })
-
-
-}else{
-    console.log('Something wrong');
- }
-  fs.writeFile('wiki.json', JSON.stringify(json, null, 2), function(err){
-            console.log('The data has been saved in a file! Look for the wiki.json file');
-          })
- })
 
